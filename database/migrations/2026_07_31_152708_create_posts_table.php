b3974397811a3b7d4bee47a9faea4fb1f0e6a11a
azzaaass <mobilelegendsazza@gmail.com>
2026-07-31 23:03:28 +0700

    create data schema

--- database/migrations/2026_07_31_152708_create_posts_table.php
+++ database/migrations/2026_07_31_152708_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->string('caption')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
