ad34b61f5753da76592cac0c5673a73433fab48d
azzaaass <mobilelegendsazza@gmail.com>
2026-08-01 05:46:20 +0700

    done post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,16 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    public function user(){
+    protected $fillable = [
+        'user_id',
+        'caption',
+        'image',
+    ];
+
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function comments(){
+    public function comments()
+    {
         return $this->hasMany(Comment::class);
     }
 
-    public function likes(){
+    public function likes()
+    {
         return $this->hasMany(Like::class);
     }
-
 }
